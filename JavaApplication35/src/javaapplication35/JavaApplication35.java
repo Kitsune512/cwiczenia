752cc3c1df0a81bfdff2472be245abbe8bf82e23
Damian <damian96_13@o2.pl>
2017-11-26 14:47:17 +0100

    Test1

--- JavaApplication35/src/javaapplication35/JavaApplication35.java
+++ JavaApplication35/src/javaapplication35/JavaApplication35.java
@@ -5,6 +5,11 @@
  */
 package javaapplication35;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author student
@@ -14,8 +19,37 @@ public class JavaApplication35 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.print("test");
+   public static void main(String[] args) {
+ 
+   FileReader fr = null;
+   String linia = "";
+
+   // OTWIERANIE PLIKU:
+   try {
+     fr = new FileReader("C:\\Users\\student\\Desktop\\plik.txt");
+   } catch (FileNotFoundException e) {
+       System.out.println("BŁĄD PRZY OTWIERANIU PLIKU!");
+       System.exit(1);
+   }
+
+   BufferedReader bfr = new BufferedReader(fr);
+   // ODCZYT KOLEJNYCH LINII Z PLIKU:
+   try {
+     while((linia = bfr.readLine()) != null){
+        System.out.println(linia);
+     }
+    } catch (IOException e) {
+        System.out.println("BŁĄD ODCZYTU Z PLIKU!");
+        System.exit(2);
+   }
+
+   // ZAMYKANIE PLIKU
+   try {
+     fr.close();
+    } catch (IOException e) {
+         System.out.println("BŁĄD PRZY ZAMYKANIU PLIKU!");
+         System.exit(3);
+        }
     }
     
 }
